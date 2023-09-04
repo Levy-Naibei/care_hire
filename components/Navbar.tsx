997ef499a7372371dfe3548b05b812fffa8c826b
Levy-Naibei <levynaibei2@gmail.com>
2023-09-04 23:48:10 +0300

    added navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import ButtonComponent from './ButtonComponent'
+import car_logo from './../public/logo.svg'
+
+const Navbar = () => {
+  return (
+    <header className="w-full absolute z-10">
+      <nav className="
+        flex justify-between max-w-[1440px]
+        mx-auto items-center sm:px-16 px-6 py-4
+        ">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src={car_logo}
+            alt="car logo"
+            height={18}
+            width={118}
+            className="object-contain"
+          />
+        </Link>
+
+        <ButtonComponent
+          title='Sign In'
+          btnType={"button"}
+          containerStyles="text-primary-blue bg-white rounded-full min-w-[130px]"
+        />
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar

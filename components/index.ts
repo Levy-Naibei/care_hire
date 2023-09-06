1e3cd234121586996f9bd24b5ae4b820c5967f13
Levy-Naibei <levynaibei2@gmail.com>
2023-09-06 13:26:23 +0300

    added filter component

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar";
+import FilterComponent from "./FilterComponent";
 
 export {
     Navbar,
     Footer,
     SearchBar,
+    FilterComponent,
 }

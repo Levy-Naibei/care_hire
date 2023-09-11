e1c92d85462cd3abcc3473c7528da06feca77b6e
Levy-Naibei <levynaibei2@gmail.com>
2023-09-11 14:36:27 +0300

    added selected and active states

--- components/SearchMake.tsx
+++ components/SearchMake.tsx
@@ -62,7 +62,22 @@ const SearchMake = ({ make, setMake }: SearchMakeProps) => {
                         `relative search-manufacturer__option 
                       ${active ? 'bg-primary-blue text-white rounded-md' : "text-gray-900"}`
                       }>
-                      {item}
+                      {
+                        ({ selected, active }) => (
+                          <>
+                            <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                              {item}
+                            </span>
+                            {selected ? (
+                              <span className={
+                                `absolute inset-y-0 left-0 flex items-center pl-3
+                                ${active ? "text-white" : "text-teal-600"}
+                                `}>
+                              </span>
+                            ) : null}
+                          </>
+                        )
+                      }
                     </Combobox.Option>
                   ))
                 )

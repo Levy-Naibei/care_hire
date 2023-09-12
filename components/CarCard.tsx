4dc0c9b273ac3aad91af608eb106f34894ef3e64
Levy-Naibei <levynaibei2@gmail.com>
2023-09-12 13:06:39 +0300

    added car rent price

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,17 +1,33 @@
 import React from 'react'
+import Image from 'next/image';
+
 import { CarProps } from '@/types';
 import ButtonComponent from './ButtonComponent';
-
+import { calculateCarRent } from '@/utils';
 interface CarCardProps {
-    car: CarProps;
+  car: CarProps;
 }
 
 const CarCard = ({ car }: CarCardProps) => {
-    const { city_mpg, combination_mpg, cylinders, displacement, drive,
-        fuel_type, highway_mpg, make, model, transmission, year } = car;
-    return (
-        <div>{drive}</div>
-    )
+  const { city_mpg, drive, make, model, transmission, year } = car;
+  const carRent = calculateCarRent(city_mpg, year);
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">{make} {model}</h2>
+      </div>
+      <p className="flex mt-6 font-extrabold text-[32px]">
+        <span className="self-start text-[14px] font-semibold">
+          $
+        </span>
+        {carRent}
+        <span className="self-end text-[14px] font-medium">
+          /day
+        </span>
+      </p>
+    </div>
+  )
 }
 
 export default CarCard;

a57f31a703a4e8cba7af44c86b36034f6089e2d5
Levy-Naibei <levynaibei2@gmail.com>
2023-09-12 12:52:35 +0300

    added calculate car rent utility function

--- utils/index.ts
+++ utils/index.ts
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 const options = {
   method: 'GET',
-  url: process.env.REACT_APP_API_BASE_URL,
-  params: { model: 'Ferrari' },
-  'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
-  'X-RapidAPI-Host': process.env.REACT_APP_API_HOST
+  url: process.env.REACT_APP_API_URL,
+  params: { model: 'camry' },
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+    'X-RapidAPI-Host': process.env.REACT_APP_API_HOST
+  }
 };
 
 export const fetchCars = async () => {
@@ -17,3 +19,18 @@ export const fetchCars = async () => {
     console.error(error);
   }
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};

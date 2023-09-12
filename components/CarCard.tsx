9de799b15830e39cf77dbc89aeb406fdd3dc0f67
Levy-Naibei <levynaibei2@gmail.com>
2023-09-12 13:43:01 +0300

    added transmission type, drive type and miles per gallon in a city

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
+import { GiSteeringWheel } from 'react-icons/gi'
 
 import { CarProps } from '@/types';
 import ButtonComponent from './ButtonComponent';
@@ -33,6 +34,32 @@ const CarCard = ({ car }: CarCardProps) => {
           priority fill
           className="object-contain"
         />
+      </div>
+      <div className="relative w-full flex mt-2">
+        <div className="flex group-hover:visible w-full justify-between text-gray">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Image src="/steering-wheel.svg" alt="steering wheel" width={20} height={20} />
+            <p className="text-[14px]">
+              {transmission == "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+        </div>
+        <div className="flex group-hover:visible w-full justify-between text-gray">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Image src="/tire.svg" alt="tire" width={20} height={20} />
+            <p className="text-[14px]">
+              {drive.toUpperCase()}
+            </p>
+          </div>
+        </div>
+        <div className="flex group-hover:visible w-full justify-between text-gray">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Image src="/gas.svg" alt="gas" width={20} height={20} />
+            <p className="text-[14px]">
+              {city_mpg} MPG
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   )

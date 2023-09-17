ed378b02f77b4f8761522f77cae8cfb005efcc5d
Levy-Naibei <levynaibei2@gmail.com>
2023-09-17 23:13:24 +0300

    added search button

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,20 @@
 "use client"
 
 import { useState } from "react"
+import Image from "next/image";
 import { SearchMake } from ".";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnfying glass"
+      height={40}
+      width={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 const SearchBar = () => {
   const [make, setMake] = useState("");
@@ -15,6 +28,7 @@ const SearchBar = () => {
           make={make}
           setMake={setMake}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
     </form>
   )

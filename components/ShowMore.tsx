dae411532c07068492746c8992a0522076aa9ff6
Levy-Naibei <levynaibei2@gmail.com>
2023-09-19 15:14:55 +0300

    reused button component for show more

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client"
+import { ShowMoreProps } from '@/types';
+import { useRouter } from 'next/navigation';
+import ButtonComponent from './ButtonComponent';
 
-const ShowMore = () => {
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => { }
+
   return (
-    <div>ShowMore</div>
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext &&
+        <ButtonComponent
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      }
+    </div>
   )
 }
 

85e83185049cfc8ebfbdc93abf0269b43bebbc7c
Levy-Naibei <levynaibei2@gmail.com>
2023-09-20 12:32:16 +0300

    added home page props

--- types/index.ts
+++ types/index.ts
@@ -1,49 +1,57 @@
 import { MouseEventHandler } from "react";
 
 export interface ButtonComponentProps {
-    btnType?: "button" | "submit";
-    title: string;
-    containerStyles?: string;
-    handleClick?: MouseEventHandler<HTMLButtonElement>;
-    textStyles?: string;
-    rightIcon?: string;
-    isDisabled?: boolean;
+  btnType?: "button" | "submit";
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
+
 export interface FilterComponentProps {
-    title: string;
-    options: OptionsProps[];
+  title: string;
+  options: OptionsProps[];
 }
+
 export interface OptionsProps {
-    title: string;
-    value: string;
+  title: string;
+  value: string;
 }
+
 export interface SearchMakeProps {
-    make: string;
-    setMake: (make: string) => void;
+  make: string;
+  setMake: (make: string) => void;
 }
+
 export interface CarProps {
-    city_mpg: number;
-    class: string;
-    combination_mpg: number;
-    cylinders: number;
-    displacement: number;
-    drive: string;
-    fuel_type: string;
-    highway_mpg: number;
-    make: string;
-    model: string;
-    transmission: string;
-    year: number;
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
 
 export interface FilterProps {
-    model: string
-    make: string,
-    fuel: string
-    limit: number,
-    year: number,
+  model: string
+  make: string,
+  fuel: string
+  limit: number,
+  year: number,
+}
+
+export interface HomeProps {
+  searchParams: FilterProps;
 }
 export interface ShowMoreProps {
-    pageNumber: number;
-    isNext: boolean;
+  pageNumber: number;
+  isNext: boolean;
 }

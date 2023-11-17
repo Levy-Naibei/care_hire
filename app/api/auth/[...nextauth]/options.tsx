130ce97dadd0481ca46b01484fea22b4a9bcdc8f
Levy-Naibei <levynaibei2@gmail.com>
2023-11-17 13:19:45 +0300

    added auth options with google provider

---
+++ app/api/auth/[...nextauth]/options.tsx
@@ -0,0 +1,32 @@
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+
+export const OPTIONS: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+
+      profile(profile: GoogleProfile) {
+        console.log("Profile Google: ", profile);
+
+        let userRole = "Google User";
+        if (profile?.email === process.env.NEXT_PUBLIC_EMAIL_ADDRESS) {
+          userRole = "admin";
+        }
+        return { ...profile, id: profile.sub, role: userRole };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.id = user.id;
+      return token;
+    },
+    async session(params) {
+      let { session, token } = params;
+      session.user = token;
+      return session;
+    },
+  },
+};
